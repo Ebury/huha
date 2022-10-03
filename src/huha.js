beb76827205f68855e12e7e3383daa988394b712
corinaper <corina.perjan@ebury.com>
2022-10-03 18:22:17 +0200

    added a comment

--- src/huha.js
+++ src/huha.js
@@ -294,6 +294,7 @@ class HuhaEvent {
         value: this.value,
       });
     } else if (typeof ga !== 'undefined') {
+      //ignoring the "value" as it needs to be an int for the event to fire, currently we are sending str 
       ga('send', 'event', this.category, this.action, this.object);
     }
   }
