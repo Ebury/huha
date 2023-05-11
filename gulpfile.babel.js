0e45942b67f0f14443641b9c676e52f1aa7c707f
Міlos Ⅽіbսlkа <mcibique@users.noreply.github.com>
2023-05-11 15:05:40 +0100

    [ONL-7491] feat: Move from CircleCI to GH Actions. (#67)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import del from 'del';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
-import packageJson from './package.json';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
@@ -11,10 +10,7 @@ const { src, dest, series } = require('gulp');
 
 const $ = gulpLoadPlugins();
 
-const VERSION = packageJson.version;
 const buildFolder = 'dist';
-const versionedBuildFolder = `${buildFolder}/${VERSION}`;
-const devBuildFolder = `${buildFolder}/dev`;
 
 const clean = () => del(['dist']);
 
@@ -40,30 +36,9 @@ const build = () => {
     .pipe($.sourcemaps.init({ loadMaps: true }))
     .pipe($.uglify())
     .pipe($.sourcemaps.write('./'))
-    .pipe(dest(buildFolder))
-    .pipe(dest(versionedBuildFolder))
-    .pipe(dest(devBuildFolder));
-};
-
-const deploy = () => {
-  const publisher = $.awspublish.create({
-    'params': {
-      'Bucket': process.env.HUHA_S3_BUCKET
-    },
-    'accessKeyId': process.env.HUHA_S3_ACCESS_KEY_ID,
-    'secretAccessKey': process.env.HUHA_S3_SECRET_ACCESS_KEY,
-  });
-
-  const headers = {
-    'Cache-Control': 'max-age=315360000, no-transform, public'
-  };
-
-  return src('dist/**/*.*')
-    .pipe(publisher.publish(headers))
-    .pipe($.awspublish.reporter());
+    .pipe(dest(buildFolder));
 };
 
 gulp.task('lint', lint);
-gulp.task('deploy', series(clean, build, deploy));
 gulp.task('build', series(clean, build));
 gulp.task('default', series(clean, build));
